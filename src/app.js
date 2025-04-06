cba451b6bf5a8a3c49183000700e4370b07cad38
Brijesh Singh <brijeshsingh9090@gmail.com>
2025-04-07 00:17:44 +0530

    Project 8th Commit All Setups Configured 1st Route Created 💕

--- src/app.js
+++ src/app.js
@@ -22,9 +22,14 @@ app.use(express.urlencoded({
 app.use(express.static('public'));
 app.use(cookieParser());
 
+// Routes Import
+ import userRouter from './routes/user.routes.js';
 
+// Routes Declaration
 
+app.use('/api/v1/users', userRouter);
 
+// http://localhost:5000/api/v1/users/register
 
 
 
